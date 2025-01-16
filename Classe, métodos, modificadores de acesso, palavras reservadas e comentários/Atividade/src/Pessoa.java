5d5ade83bb690a95dffef27c1d0cec274b00cab8
GustavoToebe <gustavotoebe4@gmail.com>
2025-01-15 22:59:12 -0300

    Atividade 2

---
+++ Classe, métodos, modificadores de acesso, palavras reservadas e comentários/Atividade/src/Pessoa.java
@@ -0,0 +1,60 @@
+public class Pessoa {
+
+    //Classe pessoa com seus atributos e contrutores
+    private String nome;
+    private Integer idade;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, Integer idade, String cpf, String email, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
