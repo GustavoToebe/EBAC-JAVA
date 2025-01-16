48caf813122e6467766d9b24e85766e8bf0e7fc6
GustavoToebe <gustavotoebe4@gmail.com>
2025-01-15 23:08:40 -0300

    Atividade media

---
+++ Variávies, tipos e operadores/Atividade nota/src/Atividade.java
@@ -0,0 +1,12 @@
+public class Atividade {
+    public static void main(String[] args) {
+        Double nota1 = 10.0;
+        Double nota2 = 4.5;
+        Double nota3 = 7.5;
+        Double nota4 = 6.5;
+
+        Double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(media);
+    }
+}
