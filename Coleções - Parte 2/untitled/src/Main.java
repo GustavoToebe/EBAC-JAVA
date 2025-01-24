69f9244bc5c78451932d05ff22edd470283487ba
GustavoToebe <gustavotoebe4@gmail.com>
2025-01-23 22:39:01 -0300

    Atividade de colecoes parte 2

---
+++ Coleções - Parte 2/untitled/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String aux = sc.nextLine();
+
+        String[] listaSup = aux.split(",");
+
+        List<String> listaMasculino = new ArrayList<>();
+        List<String> listaFeminino = new ArrayList<>();
+
+        for(String nomeEsexo : listaSup){
+            if (nomeEsexo.contains("Masculino") || nomeEsexo.contains("M")){
+                listaMasculino.add(nomeEsexo.trim());
+            }else if(nomeEsexo.contains("Feminino") || nomeEsexo.contains("F")){
+                listaFeminino.add(nomeEsexo.trim());
+            }
+        }
+
+        Collections.sort(listaMasculino);
+        System.out.println("LIsta Masculina");
+        for (String nome : listaMasculino){
+            System.out.println(nome);
+        }
+
+        Collections.sort(listaFeminino);
+        System.out.println("LIsta Feminina");
+        for (String nome : listaFeminino){
+            System.out.println(nome);
+        }
+
+    }
+}
